9d4bc357325000315091ac36d5ba4b70abcebb85
sieunkim <sieunkr@gmail.com>
2021-01-16 09:55:49 +0900

    d

--- 2-1/src/main/java/com/example/demo/DemoApplication.java
+++ 2-1/src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,5 @@
 package com.example.demo;
 
-import com.example.demo.repository.MovieRepository;
-import com.example.demo.repository.MovieRepositoryImpl;
-import com.example.demo.service.MovieService;
-import com.example.demo.service.MovieService02;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,17 +9,5 @@ public class DemoApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-
-        /*
-        MovieService movieService = new MovieService();
-        movieService.printAllTitle();
-         */
-
-        MovieService02 movieService02 = new MovieService02(new MovieRepositoryImpl());
-        movieService02.printAllTitle();
     }
 }
