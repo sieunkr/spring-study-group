7e262ec07f3ec33ffacf1f650906a514ea7f6acd
sieunkim <sieunkr@gmail.com>
2021-01-31 09:07:18 +0900

    -

--- 4-0/src/main/java/com/example/demo/provider/cache/RedisCustomCacheManager.java
+++ 4-0/src/main/java/com/example/demo/provider/cache/RedisCustomCacheManager.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 
-@Component("redisCacheManager")
+@Component("redisCustomCacheManager")
 public class RedisCustomCacheManager implements CustomCacheManager {
 
 
